48be1b1529890f76e5d15a9559ed8a49cdc7da28
ju1234 <596117054@qq.com>
2017-08-16 11:01:34 +0800

    create Page Home

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+/**
+ * intro：       index
+ * description： index
+ * author：      jufei
+ * date：        2017/8/16
+ */
+
+import React, {Component} from 'react';
+
+export default class index extends Component {
+  render() {
+    return <div>index</div>
+  }
+}
